94ffb62cf662f5f91588eb5fca1d99e852d5e182
xRoody <sasha_ermak_2002@mail.ru>
2022-03-20 10:19:55 +0300

    feat: add login page with style,login controller, white home page and home controller.

---
+++ src/main/java/com/example/lab_1/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.lab_1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "LoginPage";
+    }
+}
